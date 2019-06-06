a3764d1e347334e845db1087458caa3bab11f904
PingChang1999 <47250629+PingChang1999@users.noreply.github.com>
2019-06-06 12:02:26 -0400

    Update weather.cpp

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -11,7 +11,24 @@ Weather::Weather(std::string nm, GPS loc) :
     station_nm(nm), my_loc(loc) {
 }
 
+ostream& operator << (ostream& os, const Weather& w) {
+	os << w.get_name() << ": rating" << w.get_rating() << "," << w.my_loc;
+	return os;
+}
 
-string Weather::get_name() {
+ostream& operator << (std::ostream& os, const GPS& gps) {
+	os << "Latitude: " << gps.latitude << ", Longitude:" << gps.longitude << endl;
+	return os;
+}
+
+string Weather::get_name() const{
     return station_nm;
+}
+
+int Weather::get_rating()const {
+	return rating;
+}
+
+void Weather::set_rating(int new_rating) {
+	rating = new_rating;
 }
