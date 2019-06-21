f8b611986a83a187621e51d3ebc2fb6b0a60a7d6
PingChang1999 <47250629+PingChang1999@users.noreply.github.com>
2019-06-21 12:30:38 -0400

    Update llist.cpp

--- my_code/llist.cpp
+++ my_code/llist.cpp
@@ -1,4 +1,4 @@
-    
+
 #include <iostream>
 #include "llist.h"
 
@@ -56,3 +56,65 @@ Node* last(Node* head) {
 	else if (!head->next) return head;
 	else return last(head->next);
 }
+
+/*
+ * Delete the first node and attach head to the 2nd node:
+ * */
+bool del_head(Node*& head) {
+	if (!head) return false;
+	else {
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+		return true;
+	}
+}
+
+/*
+ * Delete the last node and set prev->next to nullptr:
+ * */
+bool del_tail(Node*& curr) {
+	if (!curr) return false;
+	else {
+		if (curr->next == nullptr) {
+			delete curr;
+			curr = nullptr;
+			return true;
+		}
+		else {
+			return del_tail(curr->next);
+		}
+	}
+}
+
+/*
+ * Duplicate the entire list -- you must not share memory!
+ * */
+Node* duplicate(Node* head) {
+	if (!head) return nullptr;
+	else {
+		return new Node (head->data, duplicate(head->next));
+	}
+}
+
+/*
+ * Reverse the list: return a brand new list with everything reversed.
+ * */
+Node* reverse(Node* curr, Node* new_next) {
+	if (!curr) return new_next;
+	else {
+		return reverse(curr->next, new Node(curr->data, new_next));
+	}
+}
+
+/*
+ * Join two lists: tack list 2 onto the end of list 1:
+ * Use existing memory.
+ * */
+Node* join(Node*& list1, Node* list2) {
+	if (!list1) return list2;
+	else {
+		last(list1)->next = list2;
+	}
+}
+
